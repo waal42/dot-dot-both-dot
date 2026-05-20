bcee72c5d516d31bc686ef6bfcc9d243d0799cc1
Filip Valášek <filipvalasek@gmail.com>
2026-05-20 10:52:31 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://waal42.github.io',
+  base: '/dot-dot-both-dot',
+  integrations: [tailwind()],
 });
