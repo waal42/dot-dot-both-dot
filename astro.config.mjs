7675df700b889d9781247eedb4b26e5e494710a1
Filip Valášek <filipvalasek@gmail.com>
2026-05-26 08:40:00 +0200

    small changes to config to make it run again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://waal42.github.io',
-  base: '/dot-dot-both-dot',
-  integrations: [tailwind()],
+  site: "https://waal42.github.io",
+  base: "/dot-dot-both-dot",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
