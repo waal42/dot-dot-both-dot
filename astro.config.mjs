0687b6a7a24b06cc79022361c9d4c9076b3446e6
Filip Valášek <filipvalasek@gmail.com>
2026-05-28 23:40:45 +0200

    design changes and content filling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://waal42.github.io",
-  base: "/dot-dot-both-dot",
+  base: "/svatba",
 
   vite: {
     plugins: [tailwindcss()],
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,18 +3,7 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        wedding: {
-          black: "#000000",
-          white: "#ffffff",
-          gray: "#aaaaaa",
-        },
-      },
-      fontFamily: {
-        sans: ["Capsuula", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
